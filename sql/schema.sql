cd5bd2e1ad1f159f8b2090869e0208a122a158b2
Jonathan Liang <jonathanliang2690123@gmail.com>
2020-06-03 11:12:22 -0700

    updated the routes files

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,22 +4,44 @@ CREATE DATABASE artistaday_db;
 
 USE artistaday_db;
 
-CREATE TABLE event {
-  startDate DATETIME NOT NULL,
-  endDate DATETIME NOT NULL,
-  title VARCHAR (30),
-  artistId INTEGER(10),
-  discount INTEGER(10)
-};
-
-CREATE TABLE artist (
-  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  category VARCHAR(30),
-  item1 VARCHAR(30),
-  item2 VARCHAR(30),
-  item3 VARCHAR(30),
-  item4 VARCHAR(30),
-  itemsPrice INTEGER(10),
-  eventId INTEGER(10)
+CREATE TABLE artists (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(30) NOT NULL,
+bio VARCHAR(30) NOT NULL,
+artistPhoto VARCHAR(30)
 );
+
+CREATE TABLE events(
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ startDate DATETIME NOT NULL,
+ endDate DATETIME NOT NULL,
+ title VARCHAR(30) NOT NULL,
+ artistId INTEGER(10) NOT NULL,
+ discount INTEGER(10) NOT NULL,
+ FOREIGN KEY (artistId) REFERENCES artists(id)
+);
+
+CREATE TABLE items(
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ category VARCHAR(30) NOT NULL,
+ price DECIMAL(10, 2) NOT NULL,
+ artistId INTEGER(10) NOT NULL,
+ eventId INTEGER(10),
+ FOREIGN KEY (eventId) REFERENCES events(id)
+);
+
+CREATE TABLE usercart(
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ item VARCHAR(30) NOT NULL,
+ price DECIMAL(10, 2) NOT NULL,
+ discount INTEGER(10) NOT NULL,
+ eventId INTEGER(10),
+ FOREIGN KEY (eventId) REFERENCES events(id)
+);
+
+INSERT INTO artists (name)
+VALUES ('Annie Leibovitz'), ('Henri Matisse'), ('Sebastian Delgado');
+
+INSERT INTO items (name)
+VALUES('Queen Elizabeth II Buckingham Palace London 2007');
