081dc4882962a2524d7169b86337024f1d98f369
LanChi <lpham2525@gmail.com>
2020-06-04 16:22:19 -0700

    revised home.js

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -2,16 +2,15 @@ const getArtwork = () => {
   axios.get(`/api/items/:${id}`)
     .then(({ data }) => {
       document.getElementById('featured').innerHTML = `
-      <img src="${item.productUrl}" 
-      alt="${item.title}}">
+      <img src="${data.productUrl}" 
+      alt="${data.title}">
       <div class="right" id="nameBox">
-          <p id="artistName">${item.artistName}
+          <p id="artistName">${data.artistName}
           </p>
           <p>Sale: ends July 8, 2020</p>
         </div>
       </div>
-          <a class="chocolat-image" href="${item.productUrl}">Lightbox Preview</a>
-          <a href="${item.productUrl}" id="link">Visit product page</a>`
+          <a href="${data.productUrl}" id="link">Visit product page</a>`
     })
     .catch(err => console.error(err))
 }
--- public/assets/js/imgur.js
+++ public/assets/js/imgur.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const imgur = require('imgur')
-const express = require("express");
-const app = express();
+
 
 imgur.setClientId(process.env.ClientId)
 
 imgur.uploadFile(document.getElementById('file').textContent)
   .then(function (json) {
     console.log(json.data.link)
-    app.post('/dashboard' function (req, res) {
+    axios.post('/dashboard' function (req, res) {
       res.send(document.getElementById('fname').textContent)
       res.send(json.data.link)
+      res.sendStatus(200)
     })
   })
   .catch(function (err) {
