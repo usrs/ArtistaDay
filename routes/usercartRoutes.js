14a387ee97f1ecc7bb1655bfb5a6919995da9c96
LanChi <lpham2525@gmail.com>
2020-06-04 13:02:16 -0700

    revised routes

--- routes/usercartRoutes.js
+++ routes/usercartRoutes.js
@@ -1,29 +1,39 @@
 const router = require('express').Router()
-const { Usercart } = require('../models')
+const { usercart } = require('../models')
 
 // GET all usercarts
 router.get('/usercarts', (req, res) => {
-  Usercart.getUsercarts(usercarts => res.json(usercarts))
+  Usercart.findAll()
+    .then(usercarts => res.json(usercarts))
+    .catch(err => console.error(err))
 })
 
 // GET one usercart
 router.get('/usercarts/:id', (req, res) => {
-  Usercart.getUsercartsWhere({ id: req.params.id }, usercart => res.json(usercart))
+  Usercart.findOne({ id: req.params.id })
+    .then(usercart => res.json(usercart))
+    .catch(err => console.error(err))
 })
 
 // POST one usercart
 router.post('/usercarts', (req, res) => {
-  Usercart.addUsercart(req.body, info => res.sendStatus(200))
+  Usercart.create(req.body)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err))
 })
 
 // PUT one usercart
 router.put('/usercarts/:id', (req, res) => {
-  Usercart.updateUsercart(req.body, { id: req.params.id }, info => res.json(info))
+  Usercart.update(req.body, { where: { id: req.params.id } })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err))
 })
 
 // DELETE one usercart
 router.delete('/usercarts/:id', (req, res) => {
-  Usercart.deleteUsercart({ id: req.params.id }, info => res.sendStatus(200))
+  Usercart.destroy({ where: { id: req.params.id } })
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err))
 })
 
 module.exports = router
