89b7f421f3e8eb4499efe1920f5b233b74d148f5
LanChi <lpham2525@gmail.com>
2020-06-05 09:55:00 -0700

    revised home.js

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -8,23 +8,29 @@ goToProductPage()
 function loadFeatured () {
   const artistArray = [
     { "Annie Leibovitz": ["Queen Elizabeth", "Rolling Stones"] }, { "Sebastio Salgado": ["elephant", "baobao tree"] }, { "Henri Matisse": ["Femme Au Chapeau", "Blue Nude"] }]
-  const featuredArtist = artistArray[Math.floor(Math.random() * artistArray.length)]
+  let featuredArtist = artistArray[Math.floor(Math.random() * artistArray.length)]
   const displayedArtwork = imageArray[Math.floor(Math.random() * imageArray.length)]
 }
 
 // function to load recent artist cards
+/* need to write function which loads the recently featured artists into cards, but every 24 hours, removes the featured artist from 4 days ago, then shifts all of the remaining cards down and adds in the featured artist from yesterday */
+
 function loadRecent (id) {
+  const d = new Date()
+  const t = d.getTime()
+  const days = Math.floor(t / (86400000))
 
-const d = new Date()
-const t = d.getTime()
-const days = Math.floor(t / (86400000))
+  const imageArray = ['Annie Leibovitz: Queen Elizabeth', 'Sebastio Salgado: elephant', 'Henri Matisse: Femme Au Chapeau 1905']
 
-const imageArray = new Array('Annie Leibovitz: Queen Elizabeth', 'Sebastio Salgado: elephant', 'Henri Matisse: Femme Au Chapeau 1905')
+  const img
+  img.src = imageArray[i]
+  const i = days % imageArray.length
 
-let i = days % imageArray.length
-
-const img = new Image()
-img.src = imageArray[i]
+  function loadNew(){
+    let featuredArtist = yesterday
+    imageArray.pop()
+    imageArray.unshift(yesterday)
+  }
 }
 window.onload = loadFeatured()
 
