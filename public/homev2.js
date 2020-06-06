c6efbc63575df1782c6783b9fbe54385cf1bee10
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-06 12:26:08 -0700

    javascript for homepage

---
+++ public/homev2.js
@@ -0,0 +1,42 @@
+const testGet = () => {
+  axios.get('/api/artists')
+  .then(({ data }) => {
+    console.log({ data } )
+
+  })
+}
+
+
+
+
+const getArtists = () => {
+  axios.get('/api/artists')
+    .then(({ data }) => {
+      data.artists.forEach(artist => {
+        let artistElem = document.createElement('div')
+        artistElem.innerHTML = `
+    <div class="col s4">
+      <div class="card medium">
+        <div class="card-image">
+          <img src="${artist.artistPhoto}">
+        </div>
+        <div class="card-content">
+          <p class="card-artist-name">:${artist.name}</p>
+        </div>
+        <div class="card-action">
+          <a class="waves-effect waves-light btn-small addBtn" a href=" /artists/{artist.name}">Visit Page</a>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    `
+        document.getElementbyId('other-featured-artists-section').append(artistElem)
+      })
+    });
+
+
+    getArtists()
+
+
+
