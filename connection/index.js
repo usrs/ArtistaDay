5b46c7971e967fd57b840f847036c0e292ae25d6
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-07 15:17:46 -0700

    updated connection

--- connection/index.js
+++ connection/index.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize')
 
+const sequelize = new Sequelize("mysql://by0u4tt1ofjlcql6:pbkcojf3iq4mqeke@sq65ur5a5bj7flas.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mkebp7xudb9ubut5", {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: true //false
+  });
 
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.LOCAL_URL)
-
+// const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.LOCAL_URL)
 
 
 module.exports = sequelize
