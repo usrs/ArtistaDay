284bb051d4bd2fbf4098b93535fdda197b20a23e
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-08 14:48:57 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 require('dotenv').config()
-db
-const http = require("http")
 
-
-//File upload
-const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 
 //Bring in express
 const express = require('express')
@@ -13,15 +7,10 @@ const { join } = require('path')
 const app = express()
 
 
-// // Create a socket server http using app
-// const server = http.createServer(app);
-// const io = socketio(server);
-
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(fileUpload());
+
 
 
 
