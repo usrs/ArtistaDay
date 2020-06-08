d3d8b001e9db9d0becd1eaf437e729e345c35be6
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-08 10:08:18 -0700

    updated dashboard and login page

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -13,7 +13,6 @@ function artistItems( )
           <div class="card large">
             <div class="card-image">
               <img src="${data.">
-              <!-- <span class="card-title">Card Title</span> -->
             </div>
             <div class="card-content">
               <p class="more-products-work-title">Artwork Title</p>
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,6 @@
 // login
 
-document.getElementById("login").addEventListener("click", (event) => {
+document.getElementById("login").addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("test");
   signIn(event.target.artistName.value);
@@ -18,7 +18,7 @@ function signIn(val1) {
 }
 
 // register
-document.getElementById("register").addEventListener("click", (event) => {
+document.getElementById("register").addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("register");
  {
