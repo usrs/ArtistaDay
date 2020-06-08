9acba9d5cbbc4a03060b8bc29509675988efebb5
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-08 16:15:48 -0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ router.use('/api', require('./artistRoutes.js'))
 router.use('/api', require('./itemRoutes.js'))
 router.use('/api', require('./eventRoutes.js'))
 router.use('/api', require('./artistUserRoutes.js'))
-router.use('/api', require('./itemRoutes2.js'))
+// router.use('/api', require('./itemRoutes2.js'))
 
 router.use(require('./viewRoutes.js'))
 
