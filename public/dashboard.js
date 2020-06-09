b0bb6682e9ed2e5f57ac967708d245ba2ece6ae5
Nathan <nathanmcneil@pm.me>
2020-06-09 16:38:31 -0700

    tried to create callback function for images

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,18 +1,19 @@
+import { response } from "express"
+
 document.getElementById('addItemForm').addEventListener("submit", function (event) {
   // prevent browser from refeshing the page
-  event.preventDefault() 
+  event.preventDefault()
 
   // convert the photo into Base64 format
   const photo = document.querySelector('input[type="file"]')
   const name = document.getElementById('artworkTitle').value
+  let link = ''
   console.log(name)
   // const photo = document.getElementById('imgPath').textContent
   console.log(photo)
   const reader = new FileReader()
   reader.readAsDataURL(photo.files[0])
   reader.onload = function () {
-    console.log(reader.result)
-
     // submit the file to the server
     fetch("/api/imgur", {
       method: "POST",
@@ -24,13 +25,30 @@ document.getElementById('addItemForm').addEventListener("submit", function (even
         name: document.getElementById('artworkTitle').value,
         photo: reader.result,
       }),
-    })
+    }).then(response => response.json())
+      .then(data => {
+        imgurCallback(data)
+      })
   }
+  console.log(link, name)
+  fetch("/api/images", {
+    method: "POST",
+    body: JSON.stringify({
+      name: document.getElementById('artworkTitle').value,
+      url: link
+    }),
+  }).catch(err => {
+    console.log(err.message)
+  })
+
   reader.onerror = function (err) {
     console.log(err);
   }
 })
-
+function imgurCallback(data, callback) {
+  link = data
+  callback(response)
+}
 // document.getElementById('addImage').addEventListener('click', event => {
 //   event.preventDefault()
 
--- routes/imgurRoute.js
+++ routes/imgurRoute.js
@@ -7,30 +7,15 @@ router.post('/imgur', (req, res) => {
   const base64Photo = photo.split(',')[1]
   imgur.setAPIUrl("https://api.imgur.com/3/");
   imgur.setClientId(process.env.ClientID);
-  console.log(imgur.getClientId())
   imgur.uploadBase64(base64Photo)
     .then(json => {
       console.log(json.data.link)
       link = json.data.link
-    })
-    .then(() => {
-      console.log(req.body.name)
-      fetch(process.env.API_URL + "/api/images", {
-        method: "POST",
-        body: JSON.stringify({
-          name: name,
-          url: link
-        }),
-      }).catch(err => {
-          console.log(err.message)
-        })
-      console.log('we did it reddit!')
-    })
-    .catch(err => {
-      console.log(err.message)
+      res.json(link)
     })
   // our file will be printed to the terminal here
-  res.end()
+  // res.end()
+  // res.json(link)
 })
 
 module.exports = router
