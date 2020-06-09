5a9565642a7eeef7cbb1fb7b9c5cecc43c25037a
Nathan <nathanmcneil@pm.me>
2020-06-09 02:33:40 -0700

    fiddled with filenames

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
-router.use('/api', require('./imgurRoute'))
-router.use('/api', require('./imageRoute'))
-router.use(require('./viewRoutes'))
+router.use('/api', require('./imgurRoute.js'))
+router.use('/api', require('./imageRoute.js'))
+router.use(require('./viewRoutes.js'))
 
 module.exports = router
