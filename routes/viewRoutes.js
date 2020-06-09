1ec1a549f550b32929d417c8e325e5ef5288ea8a
Nathan <nathanmcneil@pm.me>
2020-06-08 22:58:01 -0700

    created dashboard.html

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,28 +2,20 @@ const router = require("express").Router()
 const { Image } = require("../models")
 const { join } = require('path')
 
-router.get("/", (req, res) => {
-  Image.findAll()
-    .then(item => {
-      res.render("home", { item: item.dataValues })
-    })
-    .catch(err => console.error(err))
-})
-
-router.get("/dashboard/:id", (req, res) => {
-  Artist.findOne({ id: req.params.id, include: [Image] })
-    .then((artist) => {
-      res.render("dashboard", { artist: artist.dataValues })
-    })
-    .catch((err) => console.error(err));
-})
-
-router.get("/test", (req, res) => {
-  res.render("test")
-})
+// router.get("/dashboard/:id", (req, res) => {
+//   Artist.findOne({ id: req.params.id, include: [Image] })
+//     .then((artist) => {
+//       res.render("dashboard", { artist: artist.dataValues })
+//     })
+//     .catch((err) => console.error(err));
+// })
 
 router.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../public/index.html'))
+})
+
+router.get('/dashboard', (req, res) => {
+  res.sendFile(join(__dirname, '../public/dashboard.html'))
 })
 
 module.exports = router
